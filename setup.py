458fb5b35dd99e4a47568659e6ecd77f5dcd337c
unknown <ltskinner@crimson.ua.edu>
2019-04-11 23:49:15 -0500

    updated lisence

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='spongebob',
+      version='0.0.1',
+      description='DeSCriPTiOn (OpTiONaL)',
+      url='https://github.com/ltskinner/spongebob',
+      author='ltskinner',
+      license='MIT',
+      install_requires=['docx2txt', 'python-docx'],
+      packages=['yapywrangler'],
+zip_safe=False)
+

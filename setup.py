73f881c6c8207ddabb0304dafdd320f79b938c66
unknown <ltskinner@crimson.ua.edu>
2019-05-31 08:49:35 -0500

    updated setup for new build

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='spongebob',
-    version='0.0.3',
-    description='DeSCriPTiOn (OpTiONaL)',
-    url='https://github.com/ltskinner/spongebob',
-    author='ltskinner',
-    license='MIT',
-    install_requires=['docx2txt', 'python-docx'],
-    packages=['spongebob'],
-    package_data={'':['spongebob.jpg']},
-    zip_safe=False)
+      version='0.1.1',
+      description='DeSCriPTiOn (OpTiONaL)',
+      url='https://github.com/ltskinner/spongebob',
+      author='ltskinner',
+      license='MIT',
+      install_requires=['docx2txt', 'python-docx'],
+      packages=['spongebob'],
+      package_data={'': ['spongebob.jpg']},
+      zip_safe=False)
 
